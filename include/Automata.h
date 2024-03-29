c27327f21e37d6f57e697812350d60e04f3d1758
ActiWait <123654728+ActiWait@users.noreply.github.com>
2024-03-29 18:36:00 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -9,27 +9,27 @@
 enum class STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
 
 class Automata {
-private:
-    int cash;
-    std::vector<std::string> menu;
-    std::map<std::string, int> prices;
-    STATES state;
-    std::string selected_coffee;
+ private:
+  int cash;
+  std::vector<std::string> menu;
+  std::map<std::string, int> prices;
+  STATES state;
+  std::string selected_coffee;
 
-public:
-    Automata();
-    int getCash();
-    std::string getSelCoffee();
-    void on();
-    void off();
-    void coin(int sum);
-    void etMenu();
-    STATES getState();
-    void choice(std::string drink);
-    bool check(std::string chosen_drink);
-    void cancel();
-    void cook();
-    void finish();
+ public:
+  Automata();
+  int getCash();
+  std::string getSelCoffee();
+  void on();
+  void off();
+  void coin(int sum);
+  void etMenu();
+  STATES getState();
+  void choice(std::string drink);
+  bool check(std::string chosen_drink);
+  void cancel();
+  void cook();
+  void finish();
 };
 
 #endif  // INCLUDE_AUTOMATA_H_
